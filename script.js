1add206573e0c940d7809b5597f571ef49d09957
Mariana Pinto <mariana.vendasbb@gmail.com>
2023-03-06 17:56:33 -0300

    check password match

--- script.js
+++ script.js
@@ -52,6 +52,13 @@ function checkLength(input, min, max) {
     }
 }
 
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+    if(input1.value !== input2.value) {
+        showError(input2, 'Password do not match');
+}
+}
+
 // Get fieldname
 function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -65,4 +72,5 @@ form.addEventListener('submit', function(e) {
    checkLength(username, 3, 15);
    checkLength(password, 6, 25);
    checkEmail(email);
+   checkPasswordsMatch(password, password2);
 });
