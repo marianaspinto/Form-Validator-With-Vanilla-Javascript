2d02d949e6784e4d511f2182d006e8c77f48c8a5
Mariana Pinto <mariana.vendasbb@gmail.com>
2023-03-06 11:58:44 -0300

    function error message username

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+// Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
 
 // Event listeners 
 form.addEventListener('submit', function(e) {
